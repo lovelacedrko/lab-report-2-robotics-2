5a4ee199c17cbbaadea8d9da69f544a7c8e225cb
lovelacedrko <114182827+lovelacedrko@users.noreply.github.com>
2023-03-18 15:25:58 -0400

    Create 4 7-segment.ino

---
+++ 4 7-segment.ino
@@ -0,0 +1,15 @@
+#include "seg.h"// includes the header file for the class
+
+int my7pins[7]{2, 3, 4, 5, 6, 7, 8};
+seg my7(my7pins);
+
+void setup() {
+    my7.init();// initializes the object
+}
+
+void loop() {
+    for (int i = 0; i < 10; ++i) {
+        my7.display(i);//display value of i
+        delay(1000);// delay 1s
+    }
+}
